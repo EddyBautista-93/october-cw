ec3eaa3b091d874e29bc237749c803d8f1afe453
EddyBautista <bautista.j.eddy@gmail.com>
2021-10-03 09:26:02 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+using Internal;
+using System;
+
+namespace october_cw
+{
+    // You are given a year as integer (e. g. 2001). You should return the most 
+    // frequent day(s) of the week in that year. The result has to be a list of days
+    // sorted by the order of days in week (e. g. ['Monday', 'Tuesday'], ['Saturday', 'Sunday'], ['Monday', 'Sunday']).
+    // Week starts with Monday.
+
+    // Input: Year as an int.
+
+    // Output: The list of most frequent days sorted by the order of days in week 
+    // (from Monday to Sunday).
+    class Program
+    {
+        public static string[] MostFrequentDays(int year)
+        {
+            return new string[0];
+        }
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
